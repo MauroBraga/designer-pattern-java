af7e40c8150bbed0c3e42e288429ecb6041639d0
MauroBraga <mrb0305@gmail.com>
2021-04-26 08:54:42 -0300

    Adicionando Situação a Orçamento

--- loja/src/br/com/mrb/loja/orcamento/Orcamento.java
+++ loja/src/br/com/mrb/loja/orcamento/Orcamento.java
@@ -6,11 +6,23 @@ public class Orcamento {
 	
 	private BigDecimal valor;
 	private int quantidadeItens;
+	private String situacao;
 
 	public Orcamento(BigDecimal valor, int quantidadeItens) {
 		super();
 		this.valor = valor;
 		this.quantidadeItens = quantidadeItens;
+	}
+
+	public void  aplicarDescontoExtra(){
+		BigDecimal valorDoDesconto  BigDecimal.ZERO;
+		if(situacao.equals("EM ANALISE")){
+			valorDoDesconto = new BigDecimal("0.05");
+		}
+		if(situacao.equals("APROVAR")){
+			valorDoDesconto = new BigDecimal("0.01");
+		}
+		this.valor = this.valor.substract(valorDoDesconto);
 	}
 
 	public BigDecimal getValor() {
